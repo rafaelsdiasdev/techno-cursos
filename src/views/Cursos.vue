9b684043b3895f00ad71bd6d65ac288a7fbbad73
Rafael Dias <rafaels.dias@me.com>
2020-05-07 10:38:23 -0300

    Estilização da pagina Home e adicionado efeito de transition para exibir conteudos

--- src/views/Cursos.vue
+++ src/views/Cursos.vue
@@ -3,10 +3,11 @@
     <div v-if="loading">
       <PageLoading />
     </div>
-    <div v-if="api">
-      <h1>Cursos</h1>
-      <p>{{api}}</p>
-    </div>
+    <transition>
+      <div v-if="api">
+        <h1>{{api.titulo}}</h1>
+      </div>
+    </transition>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,25 @@
     <div v-if="loading">
       <PageLoading />
     </div>
-    <div v-if="api">
-      <h1>Home</h1>
-      <p>{{api}}</p>
-    </div>
+    <transition>
+      <div v-if="api" class="conteudo">
+        <div>
+          <h1>Sobre a {{api.titulo}}</h1>
+          <p>{{api.descricao}}</p>
+          <router-link class="btn-cursos" tag="button" to="/cursos">Cursos</router-link>
+          <div>
+            <h2>Avaliações</h2>
+            <ul>
+              <li v-for="avaliacao in api.avaliacoes" :key="avaliacao.nome">
+                <p>{{avaliacao.nome}}</p>
+                <p>{{avaliacao.descricao}}</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <img src="@/assets/aprender.png" alt="Aprenda Web Design" />
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -21,3 +36,19 @@ export default {
   }
 };
 </script>
+
+<style>
+.btn-cursos {
+  border: none;
+  background-color: #4b8;
+  border-radius: 4px;
+  color: #fff;
+  cursor: pointer;
+  padding: 15px 40px;
+  font-size: 1rem;
+  margin-top: 10px;
+  margin-bottom: 40px;
+  box-shadow: 0 4px 2px rgba(0, 0, 0, 0.1);
+  font-family: "Roboto Mono", Helvetica, Arial, sans-serif;
+}
+</style>
